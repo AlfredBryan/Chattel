1e60c799a821976648b1b27f0553f05e0f5eba11
Lord Jerry <lovinjerry004@gmail.com>
2019-10-07 14:58:09 -0700

    implement controller for creating properties

---
+++ server/controllers/property.js
@@ -0,0 +1,49 @@
+/* eslint-disable camelcase */
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const { decodeToken } = require('../helper/index');
+const { Users } = require('../models');
+
+class Property {
+  /**
+   * create property
+   * @param {object} req - api request
+   * @param {object} res - api response
+   * @param {function} next - next middleware function
+   * @return {json}
+   */
+  static async createProperty(req, res, next) {
+    const {
+      property_type,
+      num_apartment,
+      num_bathroom,
+      address,
+      rentage_amount,
+    } = req.body;
+    const user_id = decodeToken(req).id;
+
+    try {
+      await Users.create({
+        user_id,
+        property_type,
+        num_apartment,
+        num_bathroom,
+        address,
+        rentage_amount,
+      });
+    } catch (error) {
+      const err = new Error();
+      err.message = 'error occured';
+      err.details = error;
+      err.statusCode = 500;
+      return next(err);
+    }
+
+    return res.status(201).json({
+      message: 'Property created successfully',
+      statusCode: 201,
+    });
+  }
+}
+
+module.exports = Property;
