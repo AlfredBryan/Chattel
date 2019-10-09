1195131511c116c157efe7528f5e61c90538df80
Lord Jerry <lovinjerry004@gmail.com>
2019-10-08 17:01:53 -0700

    modify seeder to match new package database structure

--- server/seeders/20191007224312-packages.js
+++ server/seeders/20191007224312-packages.js
@@ -5,6 +5,8 @@ module.exports = {
     description: 'for test',
     features: ['test1', 'test2'],
     price: '50',
+    num_property: 2,
+    num_advert: 2,
   }], {}),
 
   down: (queryInterface, Sequelize) => queryInterface.bulkDelete('packages', null, {}),
