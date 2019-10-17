4b861339b8111330715e5456c04c7abe3e7d6f7d
Lord Jerry <lovinjerry004@gmail.com>
2019-10-17 14:11:00 -0700

    create model for tenants

---
+++ server/models/tenants.js
@@ -0,0 +1,60 @@
+module.exports = (sequelize, DataTypes) => {
+  const tenants = sequelize.define('tenants', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+    },
+    property_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      unique: false,
+      references: {
+        model: 'property',
+        key: 'id',
+      },
+    },
+    fullname: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: false,
+    },
+    phone_number: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: false,
+    },
+    address: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      unique: false,
+    },
+    last_payment_date: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      unique: false,
+    },
+    payment_expiry_date: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      unique: false,
+    },
+    num_years_paid: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+  }, {
+    timestamps: false,
+  });
+  // eslint-disable-next-line no-unused-vars
+  tenants.associate = function models(model) {
+    tenants.belongsTo(models.property, { foreignKey: 'property_id'});
+  };
+  return tenants;
+};
