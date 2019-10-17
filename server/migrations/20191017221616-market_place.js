87adb341ac0abdbf8f84ab88c97426f88f02b0a4
Lord Jerry <lovinjerry004@gmail.com>
2019-10-17 15:37:22 -0700

    implement migration for market place

---
+++ server/migrations/20191017221616-market_place.js
@@ -0,0 +1,56 @@
+// TODO: REFACTOR
+module.exports = {
+  up: (queryInterface, Sequelize) => queryInterface.createTable('market_place', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: Sequelize.INTEGER,
+    },
+    user_id: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      unique: false,
+      references: {
+        model: 'Users',
+        key: 'id',
+      },
+    },
+    property_type: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      unique: false,
+    },
+    location: {
+      type: Sequelize.TEXT,
+      allowNull: false,
+      unique: false,
+    },
+    price: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      unique: false,
+    },
+    num_rooms: {
+      type: Sequelize.INTEGER,
+      allowNull: true,
+      unique: false,
+    },
+    num_palour: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      unique: false,
+    },
+    num_bathroom: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      unique: false,
+    },
+    images: {
+      type: Sequelize.ARRAY,
+      allowNull: false,
+    },
+  }),
+  // eslint-disable-next-line no-unused-vars
+  down: (queryInterface, _Sequelize) => queryInterface.dropTable('market_place'),
+};
