d8dd1b14b0ee2a03087a57a32458cefdcb39b7e7
AlfredBryan <alfred.chimereze@gmail.com>
2019-10-21 12:20:48 +0100

    init properties page

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,9 @@ import React from "react";
 import Home from "./components/Home/Home";
 import { Switch, Route } from "react-router-dom";
 import Register from "./components/User/Register";
+import Features from "./components/Features/Features";
+import Testimonials from "./components/Testimonials/Testimonials";
+import Properties from "./components/Property/Properties";
 
 function App() {
   return (
@@ -9,6 +12,9 @@ function App() {
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/register" component={Register} />
+        <Route exact path="/features" component={Features} />
+        <Route exact path="/testimonials" component={Testimonials} />
+        <Route exact path="/properties" component={Properties} />
       </Switch>
     </div>
   );
